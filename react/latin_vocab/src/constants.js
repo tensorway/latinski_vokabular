f72e4cf64577c191c4b5665642c86ded8ddad7d1
tensorway <darijan.gudelj@gmail.com>
2021-02-14 19:24:45 +0100

    changed domain and api addres for production

--- react/latin_vocab/src/constants.js
+++ react/latin_vocab/src/constants.js
@@ -1,2 +1,4 @@
-export const api_name    = "http://localhost:5000/"
-export const domain_name = "http://localhost:3000/" 
+// export const api_name    = "http://localhost:5000/"
+// export const domain_name = "http://localhost:3000/" 
+export const api_name    = "http://18.191.201.204/latin_vocab/api/"
+export const domain_name = "http://18.191.201.204/latin_vocab/" 
